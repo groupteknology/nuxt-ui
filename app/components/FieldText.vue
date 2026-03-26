ca37ca0bb4aff7b547a2031ba33a2fe291eabca8
Diego Otayza <xpedition.dev@outlook.com>
2026-03-26 08:51:17 -0400

    feat: first component test

---
+++ app/components/FieldText.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+interface Props {
+  name?: string;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  name: "Default",
+});
+</script>
+
+<template>
+  <div>FieldText: {{ props.name }}</div>
+</template>
