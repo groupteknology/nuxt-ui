0591cc578134579f79c62abd808cb442dd0355cf
Diego Otayza <xpedition.dev@outlook.com>
2026-03-26 09:13:29 -0400

    feat: first component test - 2

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   components: [
     {
-      path: "~/components",
+      path: "~/app/components",
       pathPrefix: false,
       prefix: "U",
     },
